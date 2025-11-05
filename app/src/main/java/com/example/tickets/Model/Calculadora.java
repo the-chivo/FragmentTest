796080e4f00b7d216ba5b73b9081a36e04d3e24d
Pablo-gg9 <pgordogonzalez@gmail.com>
2025-11-05 12:28:52 +0100

    añadido los unitTests

---
+++ app/src/main/java/com/example/tickets/Model/Calculadora.java
@@ -0,0 +1,14 @@
+package com.example.tickets.Model;
+
+public class Calculadora {
+    public static int sumar(int n1, int n2){
+        int resultado = n1 + n2;
+        return resultado;
+    }
+
+    public static int dividir(int n1, int n2){
+        int resultado = n1 / n2;
+        if
+        return resultado;
+    }
+}
--- app/src/test/java/com/example/tickets/ExampleUnitTest.java
+++ app/src/test/java/com/example/tickets/ExampleUnitTest.java
@@ -4,14 +4,22 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import com.example.tickets.Model.Calculadora;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void testSuma(){
+        assertEquals(5, Calculadora.sumar(2,3));
+    }
+    @Test(expected = ArithmeticException.class)  //Si divides por 0 salta aritmetic exception pero si le añades el expected ya no salta.
+    public void testDivision(){
+        assertEquals(5, Calculadora.dividir(10, 0));
     }
 }
