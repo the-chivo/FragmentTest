979ec9614dec04c65864c6a31d3abf977fea9d34
Pablo-gg9 <pgordogonzalez@gmail.com>
2025-11-05 11:30:53 +0100

    Initial commit

---
+++ app/src/main/java/com/example/tickets/BlankFragment.java
@@ -0,0 +1,32 @@
+package com.example.tickets;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class BlankFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if(getActivity() instanceof MainActivity){
+
+            ((MainActivity) getActivity()).test();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tickets/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.tickets;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+    BlankFragment fragmentMain;
+    BlankFragment fragment1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transact = fragmentManager.beginTransaction();
+
+        fragmentMain = new BlankFragment();
+        fragment1 = new BlankFragment();
+        transact.replace(R.id.fragmentContainer, fragmentMain);
+        transact.commit();
+
+    }
+
+    public void test(){
+
+    }
+}
