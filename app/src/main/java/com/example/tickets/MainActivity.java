1a64c9900d0a183f83b9c91bbe5833ac54936d20
adria <titomonguer@gmail.com>
2025-11-23 14:23:33 +0100

    Preguntar por permisos de almacenamiento

--- app/src/main/java/com/example/tickets/MainActivity.java
+++ app/src/main/java/com/example/tickets/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.tickets;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.Settings;
 import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -31,8 +37,23 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        Intent intent = new Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
-        startActivity(intent);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {   //Pide los permisos de almacenamiento si no estan (andorid 11 o superior)
+            // Android 11 o superior
+            if (!Environment.isExternalStorageManager()) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                startActivity(intent);
+            }
+        }
+        else {  //Hace lo mismo pero para versiones de android anteriores
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        100);
+            }
+        }
+
+
 
         ImageButton createTicketBtn = (ImageButton) findViewById(R.id.anadirTicket);
         ImageButton listTicketBtn = (ImageButton) findViewById(R.id.verTicket);
